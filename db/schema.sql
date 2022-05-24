e48c371bd4e44c9b1beac3706054b6fa24e1cb7c
Corey Levine <coreylevine2000@gmail.com>
2022-05-23 20:56:00 -0400

    scjema updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,31 @@
--- Creates the books_db database --
+-- Creates the database --
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
--- Uses the books_db database --
+
+-- Uses the database --
 USE employee_db;
 
 
-CREATE TABLE department ();
+CREATE TABLE department (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL,
+);
 
 
-CREATE TABLE role();
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
-
-CREATE TABLE employee();
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR (30),
+  role_id INTEGER (10),
+  manager_id INTEGER (10) NULL,
+  PRIMARY KEY (id)
+);
